ec401518c8f1f460fb4a23a074f8a71eb09ea2cd
Eli Filiba <efiliba@hotmail.com>
2025-04-29 19:14:07 +1000

    Remove dist

--- dist/helpers/index.js
+++
@@ -1,3 +0,0 @@
-export { default as notFound } from "./not-found";
-export { default as onError } from "./on-error";
-export { default as serveEmojiFavicon } from "./serve-emoji-favicon";
--- dist/helpers/not-found.js
+++
@@ -1,8 +0,0 @@
-import { NOT_FOUND } from "./http-status-codes";
-import { NOT_FOUND as NOT_FOUND_MESSAGE } from "./http-status-phrases";
-const notFound = (c) => {
-    return c.json({
-        message: `${NOT_FOUND_MESSAGE} - ${c.req.path}`,
-    }, NOT_FOUND);
-};
-export default notFound;
