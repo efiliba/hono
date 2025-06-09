68028f85a8f088f3f88e5efa889fea3be2bac04e
Eli Filiba <efiliba@hotmail.com>
2025-06-09 21:55:27 +1000

    Protect tasks routes, but not when testing

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,22 +1,27 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-// import { csrf } from "hono/csrf";
-// import { jwt } from "hono/jwt";
+import { csrf } from "hono/csrf";
+import { jwt } from "hono/jwt";
 
-// import env from "@/env";
+import env from "@/env";
 import { pinoLogger } from "@/middlewares/pino-logger";
 import { defaultHook, notFound, onError, serveEmojiFavicon } from "helpers";
 
 import type { AppBindings, AppOpenAPI } from "./types";
 
-export function createRouter() {
-  return new OpenAPIHono<AppBindings>({ strict: false, defaultHook }); // ignore trailing /
-}
+export const createRouter = () =>
+  new OpenAPIHono<AppBindings>({ strict: false, defaultHook }); // ignore trailing /
 
-export function createApp() {
+export const createApp = () => {
   const app = createRouter();
 
-  // app.use("*", csrf());
-  // app.use("/tasks/*", jwt({ secret: env.JWT_SECRET, cookie: "authToken" }));
+  if (["production", "development"].includes(env.NODE_ENV)) {
+    if (env.NODE_ENV === "production") {
+      app.use("*", csrf());
+    }
+
+    app.use("/tasks/*", jwt({ secret: env.JWT_SECRET, cookie: "authToken" }));
+  }
+
   app.use(serveEmojiFavicon("😀"));
   app.use(pinoLogger());
 
@@ -24,12 +29,12 @@ export function createApp() {
   app.onError(onError);
 
   return app;
-}
+};
 
 // Create a router for tests with the same middleware and error handlers, ...
-export function createTestApp(router: AppOpenAPI) {
+export const createTestApp = (router: AppOpenAPI) => {
   const testApp = createApp();
   testApp.route("/", router);
 
   return testApp;
-}
+};
